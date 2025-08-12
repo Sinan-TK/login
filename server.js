928631213b4d9e8db2a768dd05013d543c00bfac
Muhammed Sinan T K <sinantkthonikadavath@gmail.com>
2025-08-12 19:37:04 +0530

    4 commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require("express-session");
 const app = express();
 const nocache = require('nocache');
 const ejs = require("ejs");
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
